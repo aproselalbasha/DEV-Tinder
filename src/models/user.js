8b614c279e74ee77e4b1946103458f7178cee1e2
aproselalbasha <ap.aprose@gmail.com>
2025-03-28 06:52:51 +0530

    datavalidation libirarys

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
-const Userschema = mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      minlength: 8,
-    },
-    lastName: {
-      type: String,
-    },
-    emailId: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    passWord: {
-      type: String,
-      default: "applemac786",
+const validate = require("validator");
+const Userschema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  lastName: {
+    type: String,
+  },
+  passWord: {
+    type: String,
+    validate: function (value) {
+      if (!validate.isStrongPassword(value)) {
+        throw new Error("please enter strong password");
+      }
     },
     age: {
       type: Number,
@@ -27,13 +23,25 @@ const Userschema = mongoose.Schema(
     skills: { type: [String] },
     gender: {
       type: String,
+      timestamps: true,
       validate(value) {
         if (!["male", "female", "others"].includes(value)) {
           throw new Error("invalid gender");
         }
       },
     },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: function (value) {
+        if (!validate.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
   },
-  { timestamps: true }
-);
+});
 module.exports = mongoose.model("User", Userschema);
